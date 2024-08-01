91bb3a0c41abbc073ca0c85ca8dd6f83ea5983e9
Alex Lynnyk <sasamarten082@gmail.com>
2024-08-01 11:56:17 +0300

    Update Booking

--- task-34/Booking/src/components/Button/components.jsx
+++
@@ -1,7 +0,0 @@
-import {styled} from "@mui/material";
-import {Link} from "react-router-dom";
-
-export const LinkWrapper = styled(Link)({
-  color: '#fff',
-  textDecoration: 'none',
-})
--- task-34/Booking/src/redux/actions/actions.js
+++ task-34/Booking/src/redux/actions/actions.js
@@ -1,45 +1,14 @@
 export const FETCH_HOTELS_REQUEST = 'FETCH_HOTELS_REQUEST';
-export const FETCH_HOTELS_SUCCESS = 'FETCH_HOTELS_SUCCESS';
-export const FETCH_HOTELS_FAILURE = 'FETCH_HOTELS_FAILURE';
 export const FETCH_DESTINATIONS = 'FETCH_DESTINATIONS';
-export const SET_DESTINATIONS = 'SET_DESTINATIONS';
-export const SUBMIT_DESTINATION = 'SUBMIT_DESTINATION';
-export const SUBMIT_DESTINATION_SUCCESS = 'SUBMIT_DESTINATION_SUCCESS';
-export const SUBMIT_DESTINATION_FAILURE = 'SUBMIT_DESTINATION_FAILURE';
 
 export const fetchHotelsRequest = () => ({
   type: FETCH_HOTELS_REQUEST
-});
-
-export const fetchHotelsSuccess = (hotels) => ({
-  type: FETCH_HOTELS_SUCCESS,
-  payload: hotels
-});
-
-export const fetchHotelsFailure = (error) => ({
-  type: FETCH_HOTELS_FAILURE,
-  payload: error
 });
 
 export const fetchDestinations = () => ({
   type: FETCH_DESTINATIONS
 });
 
-export const setDestinations = (destinations) => ({
-  type: SET_DESTINATIONS,
-  payload: destinations
-});
 
-export const submitDestination = (values) => ({
-  type: SUBMIT_DESTINATION,
-  payload: values
-});
 
-export const submitDestinationSuccess = () => ({
-  type: SUBMIT_DESTINATION_SUCCESS
-});
 
-export const submitDestinationFailure = (error) => ({
-  type: SUBMIT_DESTINATION_FAILURE,
-  payload: error
-});
